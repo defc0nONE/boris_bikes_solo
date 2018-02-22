7620b60f35a046a657c386d96a0d26f22bfa1e79
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-02-22 15:24:42 +0000

    Chapt 14 - set capacity to 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,13 +3,18 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def initialize(capacity = 20)
+  @bikes_in_dock = []
+  @capacity = capacity
+  end
+
   def release_bike
-    fail 'No bikes' unless @bike
+    fail 'No bikes' if @bikes_in_dock.count == 0
     @bike
   end
 
   def dock(bike)
-    fail 'Bike dock full' unless @bike.nil?
-    @bike = bike
+    fail 'Bike dock full' if @bikes_in_dock.count == @capacity
+    @bikes_in_dock << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,32 +7,35 @@ describe DockingStation do
   it 'releases a working bike' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+    expect(subject.release_bike).to eq @bikes_in_dock
   end
 # test 4 - 'dock(bike) method exists and accepts 1 argument'
   it { is_expected.to respond_to(:dock).with(1).argument }
 # test 5 - 'bike method exists'
   it { is_expected.to respond_to :bike }
 # test 6
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
+  # it 'docks a bike' do
+  #   ds = DockingStation.new
+  #   bike = Bike.new
+  #   expect(ds.dock(bike)).to respond_to :dock
+  # end
 # test 7
   it 'shows docked bikes' do
+    ds = DockingStation.new
     bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    ds.dock(bike)
+    expect(ds.bike).to eq @bikes_in_dock
   end
 # test 8
   it 'raises an error if no bikes are released' do
-    expect { subject.release_bike }.to raise_error "No bikes"
+    ds = DockingStation.new
+    expect { ds.release_bike }.to raise_error "No bikes"
   end
 # test 9
-  it 'dock only accepts 1 bike' do
-    bike1 = Bike.new
-    subject.dock(bike1)
-    bike2 = Bike.new
-    expect { subject.dock(bike2) }.to raise_error "Bike dock full"
+  it 'dock only accepts 20 bikes' do
+    ds = DockingStation.new
+    bike = Bike.new
+    20.times{ds.dock(bike)}
+    expect { ds.dock(bike) }.to raise_error "Bike dock full"
   end
 end
